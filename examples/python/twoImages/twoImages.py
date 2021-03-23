4d82a500dd3a98d6a41f62b9b43250e725a88926
M. Khaled (MBP) <eng.mk@msn.com>
2021-03-23 15:17:38 -0600

    mods

--- examples/python/twoImages/twoImages.py
+++ examples/python/twoImages/twoImages.py
@@ -1,9 +1,16 @@
+import signal
 import numpy
 import cv2
 import platform
 import ctypes
 import time
 
+
+KillMain = False
+def main_killer(signum, frame):
+    global KillMain
+    print("Exiting ... ")
+    KillMain = True
 
 def getImageDataAndResolution(imageFile):
     img = cv2.imread(imageFile, cv2.IMREAD_UNCHANGED)
@@ -28,12 +35,13 @@ def LoadNDIRestServerLib():
 
 
 def main():
+    global KillMain
+
     # load the library
     libNDIRestServer = LoadNDIRestServerLib()
     if libNDIRestServer == []:
         print("Failed to load the lib OS!")
         return
-    
 
     # create an NDISender object
     NDI_SENDER_NAME = b'My_PNG'
@@ -45,7 +53,7 @@ def main():
     [image2_data, xres2, yres2] = getImageDataAndResolution("images/image2.png")
 
     # send the images
-    while True:
+    while not KillMain:
         libNDIRestServer.NDIImageSender_setImage(ctypes.c_void_p(NDISender), ctypes.c_char_p(image1_data), ctypes.c_int(xres1), ctypes.c_int(yres1))
         time.sleep(1)
         libNDIRestServer.NDIImageSender_setImage(ctypes.c_void_p(NDISender), ctypes.c_char_p(image2_data), ctypes.c_int(xres2), ctypes.c_int(yres2))
@@ -57,4 +65,6 @@ def main():
 
 # main wrapper
 if __name__ == "__main__": 
+    signal.signal(signal.SIGINT, main_killer)
+    signal.signal(signal.SIGTERM, main_killer)
     main()
